1b10f2611803fb0daec16abbf5fd9ad6ffb87587
Jay Parikh <jayparikh265@gmail.com>
2024-10-26 15:22:50 +0530

    Added main function to handle user inputs

---
+++ src/main.py
@@ -0,0 +1,35 @@
+# Import modules
+
+
+
+
+
+def main():
+    print("************************************")
+    print("1. Add Book")
+    print("2. Book List")
+    print("3. Add Member")
+    print("4. View Member")
+    print("5. Issue Book")
+    print("6. Return Book")
+    print("7. View Transactions by Member")
+    print("***********************************")
+    choice = input("Enter your choice: ",)
+    
+    if choice == "1":
+        print("1")
+    elif choice =="2":
+        print("2")
+    elif choice =="3":
+        print("3")
+    elif choice == "4":
+        print("4")
+    elif choice == "5":
+        print("5")
+    elif choice == "6":
+        pass
+    elif choice == "7":
+        pass
+        
+if __name__ == "__main__":
+    main()
