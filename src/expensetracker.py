fbed82bd4d4e43aefa8071e9bd6a7a105047db89
Jay Parikh <jayparikh265@gmail.com>
2024-10-31 20:35:53 +0530

    Add expense, view expense test code to expense tracker

--- src/expensetracker.py
+++ src/expensetracker.py
@@ -1,11 +1,13 @@
 # Define ExpenseTracker object as class
+from expense import Expense
+from category import Category
 
 class ExpenseTracker:
     def __init__(self):
         self.expenses = []
         self.categories = []
     
-    def add_expense(self, expense):
+    def add_expense(self, expense: Expense):
         self.expenses.append(expense)
     
     def view_expenses(self):
@@ -15,4 +17,18 @@ class ExpenseTracker:
 
 
 if __name__ == "__main__":
-    print("need to add")
+    # Create a Category object
+    food_category = Category("Food")
+
+    # Create Expense objects
+    expense1 = Expense(100, food_category, "Lunch at McDonald's")
+    expense2 = Expense(200, food_category, "Dinner at Domino's")
+
+    # Track Expenses
+    tracker = ExpenseTracker()
+    tracker.add_expense(expense1)
+    tracker.add_expense(expense2)
+
+    # View expenses
+    print(tracker.expenses)
+    tracker.view_expenses()
