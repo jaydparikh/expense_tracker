3ea523613a50c3aaa3181224ab176ab5532995a6
Jay Parikh <jayparikh265@gmail.com>
2024-10-31 17:31:56 +0530

    Add while loop in main, add expense, view expense base code

--- src/main.py
+++ src/main.py
@@ -1,33 +1,48 @@
 # Import modules
-#from expense import X
+from expensetracker import ExpenseTracker
+from expense import Expense
+from category import Category
 
 
 def main():
-    print("************************************")
-    print("1. Add Expense")
-    print("2. Modify Expense")
-    print("3. Delete Expense")
-    print("4. View All Expenses")
-    print("5. View Categories")
-    print("6. Add Category")
-    print("7. View Expenses by Category")
-    print("***********************************")
-    choice = input("Enter your choice: ",)
-    
-    if choice == "1":
-        print("1")
-    elif choice =="2":
-        print("2")
-    elif choice =="3":
-        print("3")
-    elif choice == "4":
-        print("4")
-    elif choice == "5":
-        print("5")
-    elif choice == "6":
-        pass
-    elif choice == "7":
-        pass
+    while True:
+        print("************************************")
+        print("1. Add Expense")
+        print("2. Modify Expense")
+        print("3. Delete Expense")
+        print("4. View All Expenses")
+        print("5. View Categories")
+        print("6. Add Category")
+        print("7. View Expenses by Category")
+        print("8. Quit")
+        print("***********************************")
+        choice = input("Enter your choice: ",)
+        
+        if choice == "1": #Add Expense
+            user_amount = input("Enter Amount:")
+            user_category = Category("New")
+            user_description = input("Enter Description")
+            expense = Expense(user_amount,user_category,user_description)
+            tracker.add_expense(expense)
+            print("1")
+            print(expense)
+            print(tracker.expenses)
+        elif choice =="2":
+            print("2")
+        elif choice =="3":
+            print("3")
+        elif choice == "4": #View All Expenses
+            tracker.view_expenses()
+            print("4")
+        elif choice == "5":
+            print("5")
+        elif choice == "6":
+            pass
+        elif choice == "7":
+            pass
+        elif choice == "8":
+            break
         
 if __name__ == "__main__":
+    tracker = ExpenseTracker()
     main()
