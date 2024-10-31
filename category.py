dd3c24664152e9516209d7adb3a439b52e8d5576
Jay Parikh <jayparikh265@gmail.com>
2024-10-31 16:37:42 +0530

    Create and add details to category module

---
+++ category.py
@@ -0,0 +1,13 @@
+# Define category object as a class
+
+class Category():
+    
+    def __init__(self,name = "MISC",description = "NA"):
+        self.name = name
+        self.description = description
+
+    def get_name(self):
+        return self.name
+    
+    def get_description(self):
+        return self.description
