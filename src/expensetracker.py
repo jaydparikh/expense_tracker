cc3a60d33a973720a3c364b9dc50bc3992cc018c
Jay Parikh <jayparikh265@gmail.com>
2024-11-01 17:34:05 +0530

    Added view expenses by category to expense tracker

--- src/expensetracker.py
+++ src/expensetracker.py
@@ -9,13 +9,15 @@ class ExpenseTracker:
     
     def add_expense(self, expense: Expense):
         self.expenses.append(expense)
+        self.add_category(expense.category.get_name())
     
     def view_expenses(self):
         for index,item in enumerate(self.expenses):
-            print(f"Expense No.{index+1} Amount:{item.get_amount()}, Category:{item.get_category().name}, Description:{item.get_description()}")
+            print(f"Expense No.{index+1} Amount:{item.get_amount()}, Category:{item.get_category().get_name()}, Description:{item.get_description()}")
 
     def modify_expense(self,user_index, expense: Expense):
         self.expenses[user_index-1] = expense
+        self.add_category(expense.category.get_name())
 
     def delete_expense(self,user_index):
         del self.expenses[user_index-1]
@@ -28,35 +30,58 @@ class ExpenseTracker:
         for i in self.categories:
             print(i.get_name())
 
+    def view_expenses_bycategory(self, category_name):
+        for index,item in enumerate(self.expenses):
+            if item.category.get_name() == category_name:
+                print(f"Expense No.{index+1} Amount:{item.get_amount()}, Category:{item.get_category().get_name()}, Description:{item.get_description()}")
+
 
 if __name__ == "__main__":
-    # Create a Category object
-    food_category = Category("Food")
-
-    # Create Expense objects
-    expense1 = Expense(100, food_category, "Lunch at McDonald's")
-    expense2 = Expense(200, food_category, "Dinner at Domino's")
-
     # Track Expenses
     tracker = ExpenseTracker()
+
+    # View Categories
+    print("view categories")
+    tracker.view_categories()
+    print("*"*5)
+    
+    # Create a Category object
+    utility_category = Category("utilities")
+
+    # Create Expense objects
+    expense1 = Expense(100, utility_category, "Lunch at McDonald's")
+    expense2 = Expense(200, utility_category, "Dinner at Domino's")
+
+    # Add Expenses
     tracker.add_expense(expense1)
     tracker.add_expense(expense2)
     
     # View expenses
+    print("view expenses")
     tracker.view_expenses()
     print("*"*5)
 
     #Modify expense
-    expense3 = Expense(300, food_category, "Dinner at ShivSagar")
+    print("modify expenses")
+    expense3 = Expense(300, Category("food"), "Dinner at ShivSagar")
     tracker.modify_expense(1,expense3)
     tracker.view_expenses()
     print("*"*5)
 
     #Delete expense
+    print("delete expense")
     tracker.delete_expense(2)
     tracker.view_expenses()
     print("*"*5)
 
     #Add category
-    tracker.add_category("Utilities".lower())
+    print("Add Category")
+    tracker.add_category("entertainmenT".strip().lower())
+    tracker.add_category("FOOD".strip().lower())
     tracker.view_categories()
+    print("*"*5)
+
+    #View expenses by category
+    print("view expenses by category")
+    tracker.view_expenses_bycategory("food")
+    print("*"*5)
