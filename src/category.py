6fe4373ebce3d625adad2ebe24e4a6fef0328231
Jay Parikh <jayparikh265@gmail.com>
2024-11-01 15:36:37 +0530

    Removed category descrition attribute

--- src/category.py
+++ src/category.py
@@ -2,20 +2,16 @@
 
 class Category:
     
-    def __init__(self,name = "MISC",description = "NA"):
+    def __init__(self,name = "MISC"):
         self.name = name
-        self.description = description
 
     def get_name(self):
         return self.name
     
-    def get_description(self):
-        return self.description
     
 if __name__ == "__main__":
     # Create a Category object
-    food_category = Category("Food", "food,beverages,water")
+    food_category = Category("Food")
 
     # Print category details
-    print(food_category.name)
-    print(food_category.description)
+    print(food_category.get_name())
