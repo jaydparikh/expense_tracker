7bd8649fa3b9c5d2a5b0e09fddac1842f85cff2a
Jay Parikh <jayparikh265@gmail.com>
2024-11-01 17:39:53 +0530

    Added view categories, add category and view expenses by category to main loop

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from category import Category
 
 def main():
     while True:
-        print("************************************")
+        print("*"*5,"Input Options","*"*5)
         print("1. Add Expense")
         print("2. Modify Expense")
         print("3. Delete Expense")
@@ -15,41 +15,51 @@ def main():
         print("6. Add Category")
         print("7. View Expenses by Category")
         print("8. Quit")
-        print("***********************************")
+        print("*"*5, "*"*5)
         choice = input("Enter your choice: ",)
         
         if choice == "1": #Add Expense
-            print("Add Expense.Add to below existing categories or create a new one")
-            user_amount = input("Enter Amount: ")
-            user_category = Category(input("Enter Category: ").lower())
-            user_description = input("Enter Description: ").lower()
-            expense = Expense(user_amount,user_category,user_description)
+            print("Add an expense to below existing categories or create a new one")
+            tracker.view_categories()
+            user_amount = input("Enter Amount: ").strip()
+            user_category = input("Enter Category: ").strip().lower()
+            user_description = input("Enter Description: ").strip().lower()
+            expense = Expense(user_amount,Category(user_category),user_description)
             tracker.add_expense(expense)
         elif choice =="2": #Modify Expense
             print("You have made the following expenses till now")
             tracker.view_expenses()
             user_index = int(input("Which one do you want to modify: "))
-            user_amount = input("Enter Amount: ")
-            user_category = Category(input("Enter Category: ").lower())
-            user_description = input("Enter Description: ").lower()
-            expense = Expense(user_amount,user_category,user_description)
+            user_amount = input("Enter Amount: ").strip().lower()
+            user_category = input("Enter Category: ").strip().lower()
+            user_description = input("Enter Description: ").strip().lower()
+            expense = Expense(user_amount,Category(user_category),user_description)
             tracker.modify_expense(user_index,expense)
         elif choice =="3": #Delete Exepnse
             print("You have made the following expenses till now")
             tracker.view_expenses()
-            user_index = int(input("Which one do you want to delete: "))
+            user_index = int(input("Which one do you want to delete: ").strip())
             tracker.delete_expense(user_index)
         elif choice == "4": #View All Expenses
             print("You have made the following expenses till now")
             tracker.view_expenses()
-        elif choice == "5":
-            print("5")
-        elif choice == "6":
-            pass
-        elif choice == "7":
-            pass
-        elif choice == "8":
+        elif choice == "5": #View Categories
+            print("You have following categories available")
+            tracker.view_categories()
+        elif choice == "6": #Add category
+            print("You have following categories available")
+            tracker.view_categories()
+            user_category = input("What category would you like to add: ").strip().lower()
+            tracker.add_category(user_category)
+        elif choice == "7": #View Expenses by Category
+            print("You have following categories available")
+            tracker.view_categories()
+            user_category = input("What category would you like to view for: ").strip().lower()
+            tracker.view_expenses_bycategory(user_category)
+        elif choice == "8": #Quit
             break
+        else:
+            print("** Incorrect input, please try again **")
         
 if __name__ == "__main__":
     tracker = ExpenseTracker()
